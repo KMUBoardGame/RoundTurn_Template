450c4e8c954e54a6348010ea8e8c8d1a8b7c7379
[hsb031106@naver.com] <[hsb031106@naver.com]>
2022-11-21 14:07:32 +0900

    3

--- Assets/Scripts/StageDataManager.cs
+++ Assets/Scripts/StageDataManager.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class StageDataManager : MonoBehaviour
 {
+	List<int> peaches = new List<int>();
+	List<int> bananas = new List<int>();
+	List<int> plums = new List<int>();
+	List<int> limes = new List<int>();
+	List<List<int>> cards = new List<List<int>>();
+
+	[SerializeField]
+	int PlayerNum;
+
+
 	#region Round Data
 
-		int currentRound;
+
+
+
+
+	int currentRound;
 		public int CurrentRound
 		{
 			get { return currentRound; }
@@ -51,5 +65,42 @@ public class StageDataManager : MonoBehaviour
 
 
 		//¶ó¿îµå º¯¼ö ÃÊ±âÈ­
+		cards.Add(peaches);
+		cards.Add(bananas);
+		cards.Add(plums);
+		cards.Add(limes);
+
+		for (int index = 0; index < 4; index++)
+		{
+			// 1: 5°³ »ğÀÔ
+			for (int cardNum1 = 0; cardNum1 < 5; cardNum1++)
+			{
+				cards[index].Add(1);
+			}
+
+			// 2, 3, 4: 3°³ »ğÀÔ
+			for (int cardNum234 = 0; cardNum234 < 3; cardNum234++)
+			{
+				cards[index].Add(2);
+				cards[index].Add(3);
+				cards[index].Add(4);
+			}
+
+			// 5: 1°³ »ğÀÔ
+			cards[index].Add(5);
+		}
+
+
+		for (int index = 0; index < 4; index++)
+		{
+			// 1: 5°³ »ğÀÔ
+			Debug.Log(cards[index]);
+			foreach(var num in cards[index])
+            {
+				Debug.Log(num);
+            }
+
+		}
+
 	}
 }
